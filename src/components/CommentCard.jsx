4844e431a074abf262ac91e83b246c4ab0c0ba5d
Arkodia0001 <149802973+Arkodia0001@users.noreply.github.com>
2024-03-14 14:52:24 +0000

    completed task 9 deleting comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,14 +1,19 @@
-const CommentCard = ({ comment }) => {
-    const event = new Date(comment.created_at)
-    const date = event.toDateString()
+import { useContext } from "react";
+import UserContext from "../contexts/UserContext";
+import DeleteButton from "./DeleteButton";
 
-    
+const CommentCard = ({ comment, comments, setComments }) => {
+  const event = new Date(comment.created_at)
+  const date = event.toDateString()
+  const { user, setUser } = useContext(UserContext)
+
   return (
     <div className="comment_card">
       <p>{comment.body}</p>
       <p>By {comment.author}</p>
       <p>{(date)}</p>
       <p>Likes: {comment.votes}</p>
+      {user.username === comment.author && comment.comment_id !== undefined ? <DeleteButton comment={comment} comments={comments} setComments={setComments} /> : null}
     </div>
   );
 };
